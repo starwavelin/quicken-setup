de77ec3fbfd70877936d7ba460c88742d458b139
Eric Wu <eric@havenlife.com>
2021-07-04 17:55:33 -0400

    chore: in brew-cash.sh, change cmd format cask install into install --cask

--- brew-cask.sh
+++ brew-cask.sh
@@ -7,56 +7,49 @@
 
 # Install native apps
 
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+# brew install caskroom/cask/brew-cask
+# brew tap caskroom/versions
 
 
 # Env
-#brew cask install java
-#brew cask install java8
-
-brew cask install alfred
-
+#brew install --cask java
+#brew install --cask java16
 
 
 # Dev tool
-brew cask install iterm2
-#brew cask install atom
-brew cask install macvim
-brew cask install sourcetree
-brew cask install visual-studio-code
-#brew cask install sqlitestudio
-#brew cask install webstorm
-#brew cask install intellij-idea
-#brew cask install charles
-#brew cask install cyberduck
-#brew cask install mysqlworkbench
-brew cask install macdown
+brew install --cask iterm2
+#brew install --cask atom
+brew install --cask macvim
+brew install --cask sourcetree
+brew install --cask visual-studio-code
+#brew install --cask sqlitestudio
+#brew install --cask webstorm
+#brew install --cask intellij-idea
+#brew install --cask charles
+#brew install --cask cyberduck
+#brew install --cask mysqlworkbench
+brew install --cask macdown
 
-# apps
-
+# Apps
+brew install --cask alfred
 brew install --cask youdaodict
-brew cask install google-chrome
-brew cask install google-chrome-canary
-brew cask install firefox
-#brew cask install bettertouchtool
-#brew cask install macid
-brew cask install slack
-brew cask install evernote
-brew cask install dropbox
+brew install --cask google-chrome
+brew install --cask google-chrome-canary
+brew install --cask firefox
+#brew install --cask bettertouchtool
+#brew install --cask macid
+brew install --cask slack
+brew install --cask evernote
+brew install --cask dropbox
 
-# brew cask install istat-menus
-brew cask install go2shell
-#brew cask install flycut
-#brew cask install xmind
-#brew cask install tunnelblick
-#brew cask install flycut
-brew cask install wechat
-#brew cask install Kitematic 
-#brew cask install gemini
-#brew cask install cheatsheet
-brew cask install pgadmin4
-
-
-
-
+# brew install --cask istat-menus
+brew install --cask go2shell
+#brew install --cask flycut
+#brew install --cask xmind
+#brew install --cask tunnelblick
+#brew install --cask flycut
+brew install --cask wechat
+#brew install --cask Kitematic 
+#brew install --cask gemini
+#brew install --cask cheatsheet
+brew install --cask pgadmin4
