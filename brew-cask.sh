3c3557cf6ebaa88a83d7fa8ae8c1faab5a535874
starwavelin <benjaminxianlin@gmail.com>
2021-07-05 12:46:54 -0400

    chore: update need to install apps in brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -18,38 +18,47 @@
 
 # Dev tool
 brew install --cask iterm2
-#brew install --cask atom
-brew install --cask macvim
+brew install --cask atom
 brew install --cask sourcetree
 brew install --cask visual-studio-code
-#brew install --cask sqlitestudio
-#brew install --cask webstorm
-#brew install --cask intellij-idea
-#brew install --cask charles
-#brew install --cask cyberduck
-#brew install --cask mysqlworkbench
-brew install --cask macdown
+brew install --cask dbeaver-community
+brew install --cask postman
+brew install --cask insomnia
+# brew install --cask macvim
+# brew install --cask sqlitestudio
+# brew install --cask webstorm
+# brew install --cask intellij-idea
+# brew install --cask charles
+# brew install --cask cyberduck
+# brew install --cask mysqlworkbench
+# brew install --cask macdown
 
 # Apps
 brew install --cask alfred
+brew install --cask beyond-compare
+brew install --cask the-unarchiver
 brew install --cask youdaodict
-brew install --cask google-chrome
-brew install --cask google-chrome-canary
+brew install --cask sogouinput
+# brew install --cask google-chrome
+# brew install --cask google-chrome-canary
 brew install --cask firefox
+brew install --cask slack
+brew install --cask iina
+brew install --cask rectangle
+brew install --cask drawio
 #brew install --cask bettertouchtool
 #brew install --cask macid
-brew install --cask slack
-brew install --cask evernote
-brew install --cask dropbox
+# brew install --cask evernote
+# brew install --cask dropbox
 
 # brew install --cask istat-menus
-brew install --cask go2shell
+# brew install --cask go2shell
 #brew install --cask flycut
 #brew install --cask xmind
 #brew install --cask tunnelblick
 #brew install --cask flycut
-brew install --cask wechat
+# brew install --cask wechat
 #brew install --cask Kitematic 
 #brew install --cask gemini
 #brew install --cask cheatsheet
-brew install --cask pgadmin4
+# brew install --cask pgadmin4
